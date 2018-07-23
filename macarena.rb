a5a455b88e0b0ec78b17af9d151d8682068be74e
gracehillen <grahillen19@ahastars.org>
2018-07-23 21:08:13 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -23,7 +23,7 @@ puts "Step 5: Place your right hand on your left shoulder."
 
 
 # Step 6: FIXME
-puts "Step : Bring your right hand up and around to the back of your head."
+puts "Step 6: Bring your right hand up and around to the back of your head."
 
 puts "Step 6: Left arm out in front of you, palm facing down."
 
